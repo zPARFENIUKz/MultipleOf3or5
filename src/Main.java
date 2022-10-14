b705e4e6e38ab55c7bb9cac20d2093dda414a55b
Gregory <parfeniuk33@gmail.com>
2022-10-14 23:37:50 +0300

    Simple test

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args){
+        System.out.println(MultipleOf3Or5.solution(10));
+    }
 }
--- src/MultipleOf3Or5.java
+++ src/MultipleOf3Or5.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class MultipleOf3Or5 {
+import java.util.stream.IntStream;
+
+public class MultipleOf3Or5 {
+    public static int solution(final int number) {
+        if (number < 3) return 0;
+        return IntStream.range(3, number).filter(value -> value % 3 == 0 || value % 5 == 0)
+                .sum();
+    }
 }
